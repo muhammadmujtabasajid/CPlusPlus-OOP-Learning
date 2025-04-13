3be9f68e5981e9f3697ca6881681b409df0824af
M.Mujtaba Sajid <mujtabasajid00@gmail.com>
2025-04-13 06:22:26 +0500

    Update total_cars_static.cpp

--- total_cars_static.cpp
+++ total_cars_static.cpp
@@ -1,3 +1,5 @@
+//This code demonstrates the use of static data members in C++ using a class that keeps track of the total number of cars created. It's a simple OOP example for understanding how static variables behave across multiple objects.
+
 #include<iostream>
 using namespace std;
 class cars
